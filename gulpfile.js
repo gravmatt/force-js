c32435ea94e24bcaaab0058fd425a8ea78f83094
Rene Tanczos <gravmatt@gmail.com>
2019-03-17 21:52:40 +0100

    remove file watch and build by default

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 
 const gulp = require('gulp');
 const rename = require('gulp-rename');
-const path = require('path');
 const uglify = require('gulp-uglify');
 const saveLicense = require('uglify-save-license');
 const sourcemaps = require('gulp-sourcemaps');
@@ -27,12 +26,4 @@ gulp.task('js', () => {
     .pipe(gulp.dest('./src'));
 });
 
-gulp.task('js:watch', function() {
-    gulp.watch(['assets/js/index.js'], gulp.task('js'))
-    .on('change', function(event) {
-        console.log('File ' + path.basename(event) + ' was changed! Running tasks...');
-    });
-});
-
-gulp.task('build', gulp.parallel('js'));
-gulp.task('default', gulp.parallel('js:watch'));
+gulp.task('default', gulp.parallel('js'));
